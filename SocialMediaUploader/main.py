fb44a8a9039989527ae05b8dc9869e2382d84a43
Shikher-jain <shikherjain786@gmail.com>
2026-06-29 02:23:12 +0530

    change file name

---
+++ SocialMediaUploader/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from app import app
+import os
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.app:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8000)),
+        reload=False
+    )
